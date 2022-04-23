e71d96d983142e9dfd70e5e5a4bef1a9d02ae3f8
Dindihub <sadindi03@gmail.com>
2022-04-23 11:47:01 +0300

    Doc-Create user class

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+'''
+class creates instance for user accounts
+'''
+user_list=[]
+    def __init__(self,user_name,user_password):
+        self.user_name=user_name
+        self.user_password=user_password
+
+    
