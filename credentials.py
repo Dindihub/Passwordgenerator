cb1659e221ba0b893d7baff357ffcdb557c228e7
Dindihub <sadindi03@gmail.com>
2022-04-25 00:19:28 +0300

    Fix-correct code

--- credentials.py
+++ credentials.py
@@ -1,36 +1,53 @@
+
+import string
+from random import choice
 class Credentials:
     '''
     Class that creates new instances of credentials account
     ''' 
-    account=[]
-    def __init__(self,account_name,account_username,account_password_):
+    accounts=[]
+    def __init__(self,account_name,account_username,account_password):
         '''
         defines properties for object self
         ''' 
         self.account_name=account_name
         self.account_username=account_username
-        self.account_password_=account_password_
+        self.account_password=account_password
 
     def save_account(self):
 
         '''
         save_account method saves new instances of credentials
         ''' 
-
-        Credentials.account.append(self)
+        
+        Credentials.accounts.append(self)
 
     def delete_account(self):
         '''
         delete_account method deletes credentials
         '''
-        Credentials.account.remove(self)
+        Credentials.accounts.remove(self)
 
     @classmethod
-    def find_account_name_account(cls,account_name):
+    def find_account_by_name(cls,account_name):
         '''
         Method returns list of accounts
         '''
 
         for account in cls.accounts:
             if account.account_name == account_name:
-                return cls.accounts
+                return account
+
+    @classmethod
+    def generate_password(cls):
+        '''
+        Method that generates a random alphanumeric password
+        '''
+        # Length of the generated password
+        size = 8
+        # Generate random alphanumeric 
+        alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+        # Create password
+        password = ''.join( choice(alphanum) for num in range(size) )
+        
+        return password
